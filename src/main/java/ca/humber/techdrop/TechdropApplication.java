46e99d8e18c8fe69794d6976ac2e586e693be5c3
Kanwarnoor Singh <kanwr01@mac.home>
2026-06-24 22:23:12 -0400

    Initial Spring Boot project

---
+++ src/main/java/ca/humber/techdrop/TechdropApplication.java
@@ -0,0 +1,13 @@
+package ca.humber.techdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechdropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/humber/techdrop/TechdropApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.humber.techdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechdropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
